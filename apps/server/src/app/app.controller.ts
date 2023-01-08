c24efa1875fee1d26ba1f04eda050e1fe48888ba
KANGHO <kangho@bluehole.net>
2023-01-08 21:55:32 +0900

    supabase 로그 가져오는 api 추가

--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -1,13 +1,38 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { SupabaseService } from '../services/supabase';
 
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private supabaseService: SupabaseService
+  ) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  @Get('test')
+  async getTest() {
+    const client = this.supabaseService.getClient();
+    const { data, error } = await client.from('foundation_version').select('*');
+    return data;
+  }
+
+  @Get('find-version/:version')
+  async getFoundationVersion(@Param() params) {
+    const { version } = params;
+    console.log('version : ', version);
+
+    const client = this.supabaseService.getClient();
+    const { data, error } = await client
+      .from('foundation_version')
+      .select('*')
+      .eq('version', version)
+      .limit(1);
+    return data?.[0] ?? null;
   }
 }

8e657b5eff9274938dc121e43c1ef3d3e3cbe204
KANGHO <kangho@bluehole.net>
2023-01-11 23:32:19 +0900

    test api 추가

--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -1,13 +1,23 @@
+import { HttpService } from '@nestjs/axios';
 import { Controller, Get } from '@nestjs/common';
-
-import { AppService } from './app.service';
+import { firstValueFrom } from 'rxjs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly httpService: HttpService) {}
 
   @Get()
   getData() {
-    return this.appService.getData();
+    return {
+      status: 'ok',
+    };
+  }
+
+  @Get('github')
+  async githubActions() {
+    const { data } = await firstValueFrom(
+      this.httpService.get('https://api.github.com/gists')
+    );
+    return data;
   }
 }

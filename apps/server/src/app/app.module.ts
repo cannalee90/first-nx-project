176b7cd596a6c2fa3961538b71d4e6ebed57ef98
KANGHO <kangho@bluehole.net>
2023-01-11 23:09:08 +0900

    install axios

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { FoundationVersion } from './controllers/foundtaion-version';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), HttpModule],
   controllers: [AppController, FoundationVersion],
   providers: [AppService, SupabaseService],
 })

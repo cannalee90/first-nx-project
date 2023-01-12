f0691849ddee6ce80d1d99c85f56b0c3f0402e3a
KANGHO <kangho@bluehole.net>
2023-01-13 04:12:15 +0900

    add github action api

--- apps/server/src/app/app.controller.ts
+++ apps/server/src/app/app.controller.ts
@@ -1,10 +1,14 @@
 import { HttpService } from '@nestjs/axios';
 import { Controller, Get } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { firstValueFrom } from 'rxjs';
 
 @Controller()
 export class AppController {
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly config: ConfigService
+  ) {}
 
   @Get()
   getData() {
@@ -15,9 +19,27 @@ export class AppController {
 
   @Get('github')
   async githubActions() {
+    console.log(this.config.get('GITHUB_TOKEN'));
     const { data } = await firstValueFrom(
-      this.httpService.get('https://api.github.com/gists')
+      this.httpService.post(
+        'https://api.github.com/repos/cannalee90/first-nx-project/actions/workflows/45167464/dispatches',
+        {
+          ref: 'main',
+          inputs: {
+            version: '1.0.0',
+          },
+        },
+        {
+          headers: {
+            Accept: 'application/vnd.github+json',
+            Authorization: `Bearer ${this.config.get('GITHUB_TOKEN')}`,
+            'X-GitHub-Api-Version': '2022-11-28',
+          },
+        }
+      )
     );
     return data;
   }
 }
+
+

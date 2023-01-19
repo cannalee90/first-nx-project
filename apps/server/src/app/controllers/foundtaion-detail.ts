41dfc335fb5f0964a236bd9f6c55c62b37686161
KANGHO <kangho@bluehole.net>
2023-01-19 09:53:39 +0900

    foundation detail 추가

--- apps/server/src/app/controllers/foundtaion-detail.ts
+++ apps/server/src/app/controllers/foundtaion-detail.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { SupabaseService } from '../services/supabase';
 
 @Controller('foundation-detail')
@@ -13,27 +13,32 @@ export class FoundationDetail {
     if (!version) {
       throw new Error('Version is required');
     }
-    const { data } = await this.client
+    const { data, error: versionInsertError } = await this.client
       .from('foundation_version')
       .insert({ semantic_version: version, status: false })
       .select();
 
-    if (!data[0]) {
+    if (versionInsertError) {
       throw new Error('Transaction failed to insert');
     }
 
-    const { data: ret } = await this.client.from('foundation_detail').insert({
-      typography: body.typography,
-      color: body.color,
-      spacing: body.spacing,
-      foundation_id: data[0].id,
-    });
+    const { data: ret, error: detailInsertError } = await this.client
+      .from('foundation_detail')
+      .insert({
+        typography: body.typography,
+        color: body.color,
+        spacing: body.spacing,
+        foundation_id: data[0].id,
+        layout: body.layout,
+      });
 
-    if (!ret[0]) {
+    if (detailInsertError) {
       throw new Error('Transaction failed to insert');
     }
 
-    return ret;
+    console.log(data, ret);
+
+    return data[0];
   }
 
   @Get(':foundation_id')
